a71f741376bdcdd23c74e8673a23ee1ae6583b25
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2025-05-28 13:06:00 +0300

    message

--- src/apiFrontConfig.js
+++ src/apiFrontConfig.js
@@ -2,8 +2,8 @@
 // const apiBaseFrontUrl = 'http://localhost:3000';
 
 //For Live Server
-const apiBaseFrontUrl = 'https://heroku-front-15b8ce0b9404.herokuapp.com';
-
+// const apiBaseFrontUrl = 'https://heroku-front-15b8ce0b9404.herokuapp.com';
+const apiBaseFrontUrl = 'http://app.healthiq-registry.online';
 
 
 export default apiBaseFrontUrl;
