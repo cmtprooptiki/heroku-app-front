c5cc6eb864e9917a761edf70b555208fe566fe44
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2025-05-28 12:53:19 +0300

    message

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -3,6 +3,7 @@
 
 //For live
 //For heroku live
-const apiBaseUrl = 'https://salty-headland-03110-6f6b16cfd8df.herokuapp.com';
+// const apiBaseUrl = 'https://salty-headland-03110-6f6b16cfd8df.herokuapp.com';
+const apiBaseUrl = 'http://api.healthiq-registry.online';
 
 export default apiBaseUrl;
